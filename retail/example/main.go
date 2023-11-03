273535263a1cf13f015ead5aea5eabdbe673faac
xikangfang <fangxikang@bytedance.com>
2023-11-03 15:47:04 +0800

    feat(*): change example desc

--- retail/example/main.go
+++ retail/example/main.go
@@ -80,8 +80,8 @@ func init() {
 		//CallerConfig(callerConfig). // Optional.
 		//MetricsConfig(metricsConfig). // Optional.
 		//Schema("https"). // Optional.
-		//MainHost("xxx.xxx.com").                       //
-		Hosts([]string{"rec-api-sg1.recplusapi.com"}). // Optional.
+		//Hosts([]string{"rec-api-sg1.recplusapi.com"}). // Optional.
+		//MainHost("xxx.xxx.com").                     // Optional. Set one of the hosts set by '.hosts()' as the primary one.
 		Build()
 	if err != nil {
 		panic(fmt.Sprintf("byteplus rec sdk init err:%s", err.Error()))
